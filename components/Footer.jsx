7496b53453ea4a01de154dfbfb26fd0f9fc260b7
ledsouq <ledsouq@gmail.com>
2025-06-18 17:51:41 +0530

    Footer alignment

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,20 +13,22 @@ const Footer = () => {
         />
       </div>
 
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-8 relative z-10">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-10 relative z-10">
         {/* Logo & Description */}
         <div className="md:col-span-2">
           <Image
             src="/images/footer-logo.png"
             alt="Sysway Logo"
-            width={250}
+            width={300}
             height={60}
-            className="mb-6"
+            className="mb-3"
           />
-          <p className="text-sm mb-4 leading-relaxed">
-            Sysway Trading & Engineering is a locally established company in the year 1999, with a competent team of
-            highly skilled professional engineers, technicians and craftsmen in each respective field.
+          <p className="text-sm mb-4 leading-relaxed whitespace-pre-line">
+            Sysway Trading & Engineering is a locally established company in the
+            year 1999, with a competent team of highly skilled professional engineers,
+            technicians and craftsmen in each respective field.
           </p>
+
 
           <div className="flex gap-2 items-start">
             <div className="mb-2 flex gap-2 items-start">
@@ -39,9 +41,10 @@ const Footer = () => {
                 />
               </div>
               <div>
-                <p className="font-semibold">Office Address</p>
-                <p className="text-sm">
-                  Blk 3015A Ubi Road 1 #04-10 Kampong Ubi Industrial Estate Singapore 408705
+                <p className='text-[#D0FFE7]'>Office Address</p>
+                <p className="text-sm whitespace-pre-line">
+                  Blk 3015A Ubi Road 1 #04-08 Kampong Ubi Industrial
+                  Estate Singapore 408705
                 </p>
               </div>
             </div>
@@ -50,7 +53,7 @@ const Footer = () => {
           <div className="flex gap-2 items-start">
             <Image src="/images/location.png" alt="Address Icon" width={24} height={24} />
             <div>
-              <p className="font-semibold">Workshop Address</p>
+              <p className='text-[#D0FFE7]'>Workshop Address</p>
               <p className="text-sm">110 Neythal Road, Singapore 628597</p>
             </div>
           </div>
@@ -58,7 +61,7 @@ const Footer = () => {
 
         {/* Certification Column */}
         <div className="md:col-span-3">
-          <p className="font-semibold mb-4 flex items-center gap-2">
+          <p className="font-[plus jakarta sans] text-[#D0FFE7] mb-4 flex items-center gap-2">
             <Image src="/images/certificate-logo.png" alt="Certified Icon" width={46} height={46} />
             We are a Certified Manufacturers
           </p>
@@ -69,11 +72,11 @@ const Footer = () => {
             <Image src="/images/cert4.png" alt="ABS" width={146} height={88} />
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
             {/* Quick Links */}
             <div>
-              <p className="font-semibold mb-2">Quick Links</p>
-              <ul className="space-y-1 text-sm">
+              <p className="text-[#D0FFE7] mb-3">Quick Links</p>
+              <ul className="space-y-3 text-sm">
                 <li>Home</li>
                 <li>About us</li>
                 <li>Products</li>
@@ -86,8 +89,8 @@ const Footer = () => {
 
             {/* Products */}
             <div>
-              <p className="font-semibold mb-2">Products</p>
-              <ul className="space-y-1 text-sm">
+              <p className="text-[#D0FFE7] mb-3">Products</p>
+              <ul className="space-y-3 text-sm">
                 <li>Air Receiver</li>
                 <li>Steam Receiver</li>
                 <li>Hydrophore Tank System</li>
@@ -100,20 +103,20 @@ const Footer = () => {
 
             {/* Contact */}
             <div>
-              <p className="font-semibold mb-2">Contact</p>
+              <p className="text-[#D0FFE7] mb-3">Contact</p>
               <div className="flex items-center text-[#D0FFE7] gap-2 text-sm mb-2">
                 <Image src="/images/phone.png" alt="Phone" width={16} height={16} />
                 Mobile Number
               </div>
-              <div className="flex items-center gap-2 text-sm mb-2">+65 9338 8219</div>
-              <div className="flex items-center text-[#D0FFE7] gap-2 text-sm mb-1">
+              <div className="flex items-center gap-2 text-sm mb-4">+65 9338 8219</div>
+              <div className="flex items-center text-[#D0FFE7] gap-2 text-sm mb-2">
                 <Image src="/images/email.png" alt="Email" width={16} height={16} />
                 Email Address
               </div>
               <div className="flex items-center gap-2 text-sm mb-1">yawsongchan@sysway.com.sg</div>
               <div className="flex items-center gap-2 text-sm mb-4">max@sysway.com.sg</div>
-              <div className="flex items-center gap-2 text-sm">
-                <Image src="/images/social-media.png" alt="Social" width={20} height={20} />
+              <div className="flex items-center gap-2 text-sm text-[#D0FFE7]">
+                <Image src="/images/social-media.png" alt="Social" width={16} height={16} />
                 Social Media
               </div>
               <div className="flex gap-3 mt-4">
